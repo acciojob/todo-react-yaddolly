3ff42ded0e3efa16e5b9cabb75c0f376d03b5815
AAKASH _YADAV <yaddolly>
2024-09-29 11:52:12 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,48 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+
+  const[todos, setTodos] = useState([])
+  const[text, setText] = useState("")
+
+  function deleteBtnHandler(){
+
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h3>To-Do List</h3>
+        <input value={text} onChange={(e) => {
+          setText(e.target.value)
+        }} />
+        <button onClick={(e) => {
+          if(!text) return
+
+          const newTodo = {
+            task : text,
+            id : uuidv4()
+          }
+
+          setTodos([...todos, newTodo])
+
+          setText("")
+        }}>Add Todo</button>
+
+        <ul>
+          {todos && todos.map((item) => {
+            return <li style={{listStyle : "none"}} key={item.id}>{item.task}  <button onClick={() => {
+              const narr = todos.filter((todo) => {
+                return todo.id != item.id
+              })
+
+              setTodos(narr)
+            }}>Delete</button></li>
+          })}
+        </ul>
     </div>
   )
 }
